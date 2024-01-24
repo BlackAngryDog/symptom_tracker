08f38c78b983055007b589afe35f0a98aece4092
Dan Riley <Dan.Riley@Blatchford.co.uk>
2024-01-24 20:50:18 +0000

    timeline graph setup tests

--- lib/widgets/line_chart_new.dart
+++ lib/widgets/line_chart_new.dart
@@ -12,9 +12,9 @@ class LineDataChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: DataProcessManager.getSummary(),
+      future: DataProcessManager.getTimeLine(DateTime.now().subtract(const Duration(days: 7)), DateTime.now()),
       builder: (context, snapshot) => snapshot.hasData
-          ? buildSymptomsOverTimeChart(snapshot.data as List<DataLogSummary>)
+          ? buildSymptomsAdvOverTimeChart(snapshot.data as List<LogTimeLineEntry>)
           : const Center(
               child: CircularProgressIndicator(),
             ),
@@ -22,7 +22,77 @@ class LineDataChart extends StatelessWidget {
     // TODO - Build chart data from map
   }
 
-  Widget buildSymptomsOverTimeChart(List<DataLogSummary> data) {
+  Widget buildSymptomsAdvOverTimeChart(List<LogTimeLineEntry> data) {
+
+    List<LineChartBarData> dataList = [];
+    List<Color> colours = [Colors.blue, Colors.red,Colors.yellow, Colors.green, Colors.orange, Colors.pink, Colors.purple];
+    List<String> days = ["1","2","2","4","5","6","7"];
+    // get array of colours
+    List<String> symptoms = [];
+    for (var log in data){
+      if (symptoms.contains(log.title)) continue;
+      symptoms.add(log.title);
+    }
+
+    // TODO - need to build up a data set for all days between start and end with filler data for missing days
+
+    for (var option in symptoms) {
+
+      //diet.addAll(map[option]!.where((e) => !diet.contains(e.diet) ).map((e) => e.diet));
+
+      var chartData = LineChartBarData(
+        isCurved: true,
+        color: colours[0],
+        preventCurveOverShooting: true,
+        barWidth: 3,
+        isStrokeCapRound: true,
+        dotData: FlDotData(show: true),
+        belowBarData: BarAreaData(show: false),
+        spots: [],
+        //show: option == "Fits", // Todo add filters
+
+      );
+
+      var timeline = data.where((element) => element.title == option).toList();
+
+      int day = 7;
+      while (++day >= 7){
+        // Get log from timeline closes to datetime.now - day to get value
+        var greater = timeline.toList()..sort((a,b) => a.date.difference(DateTime.now()).inDays);
+
+        var x = day.toDouble();
+        var y = greater.firstOrNull!.value;
+        chartData.spots.add(FlSpot(x, y));
+      }
+
+      dataList.add(chartData);
+
+
+    }
+
+
+
+    // bottom time
+    // left symptoms
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: LineChart(
+        LineChartData(
+          lineTouchData: lineTouchData1,
+          gridData: gridData,
+          titlesData: getTitles(bottomTitles: days, leftTitles: ["0","1","2","3","4","5"]),
+          borderData: borderData,
+          lineBarsData: dataList,
+          minX: 0,
+          maxX: days.length.toDouble() - 1,
+          maxY: 5, // todo - need to adjust these based on visible data
+          minY: 0,
+        ),
+      ),
+    );
+  }
+
+  Widget buildSymptomsAdvOverDietChart(List<DataLogSummary> data) {
 
     List<LineChartBarData> dataList = [];
     List<Color> colours = [Colors.blue, Colors.red,Colors.yellow, Colors.green, Colors.orange, Colors.pink, Colors.purple];
@@ -48,7 +118,7 @@ class LineDataChart extends StatelessWidget {
         dotData: FlDotData(show: true),
         belowBarData: BarAreaData(show: false),
         spots: [],
-        show: option == "Fits", // Todo add filters
+        //show: option == "Fits", // Todo add filters
 
       );
 
